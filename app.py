dd83062020491cd48dadb3daab918b14dcd0782f
vishesh-yadav-hear <visheshyadavhear@gmail.com>
2025-08-01 12:14:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request
-from flask_socketio import SocketIO, send, emit
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hacker_h_ham'
@@ -12,10 +12,7 @@ def index():
 @socketio.on('message')
 def handle_message(msg):
     print(f"Message: {msg}")
-    send(msg, broadcast=True)  # sab users ko message bhejna
-
-
-
+    emit('message', msg, broadcast=True)  # sabhi users ko bhejna
 
 if __name__ == '__main__':
     socketio.run(app, debug=True, allow_unsafe_werkzeug=True)
