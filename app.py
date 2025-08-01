1960c84557bf065bedd49323e65e3e0a51bd06cf
vishesh-yadav-hear <visheshyadavhear@gmail.com>
2025-08-01 11:34:33 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO, send, emit
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hacker_h_ham'
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('message')
+def handle_message(msg):
+    print(f"Message: {msg}")
+    send(msg, broadcast=True)  # sab users ko message bhejna
+
+
+
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True, allow_unsafe_werkzeug=True)
